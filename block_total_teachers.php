9fcdef9793d3c90615d946a1f75644bd6336e563
parul <cseprs@gmail.com>
2023-06-15 12:15:14 +0530

    next blocks

---
+++ block_total_teachers.php
@@ -0,0 +1,75 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Form for editing HTML block instances.
+ *
+ * @package   block_total_teachers
+ 
+
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+class block_total_teachers extends block_base {
+
+    function init() {
+        $this->title = get_string('pluginname', 'block_total_teachers');
+    }
+
+    function has_config(){
+        return true;
+
+  }
+
+
+ function get_content() {
+    global $DB;
+      
+       if ($this->content  !== NULL) {
+           return $this->content;
+       }
+
+       
+
+
+
+
+     $countteachers='';
+
+      
+    
+     $enrollteachers = $DB->get_records_sql("
+
+SELECT ra.userid
+
+FROM {role_assignments} ra
+JOIN {context} ctx ON ra.contextid = ctx.id
+
+
+where ra.roleid = 3");
+
+$countteachers = count($enrollteachers);
+
+
+
+$this->content = new stdClass;
+$this->content->text = "$countteachers";
+$this->content->footer = "";
+
+
+    }
+
+}
